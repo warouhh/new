f5e54e00ba5ea9da68e0db41b6e8697dc380fdfe
warouhh <136374223+warouhh@users.noreply.github.com>
2024-07-16 00:50:57 +0700

    Update restore.sh

--- menu/restore.sh
+++ menu/restore.sh
@@ -1,76 +1,43 @@
 #!/bin/bash
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-colornow=$(cat /etc/rmbl/theme/color.conf)
-export NC="\e[0m"
-export YELLOW='\033[0;33m';
-export RED="\033[0;31m"
-export COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-export COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
-WH='\033[1;37m'
-ipsaya=$(wget -qO- ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/awanklod/izin_new/main/ip"
-checking_sc() {
-useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
-if [[ $date_list < $useexp ]]; then
-echo -ne
-else
-systemctl stop nginx
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1│ $NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  $COLOR1│"
-echo -e "$COLOR1│   ${yl}Your VPS${NC} $ipsaya \033[0;36mHas been Banned${NC}      $COLOR1│"
-echo -e "$COLOR1│     ${yl}Buy access permissions for scripts${NC}          $COLOR1│"
-echo -e "$COLOR1│             \033[0;32mContact Your Admin ${NC}                 $COLOR1│"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-exit
-fi
-}
-checking_sc
+# SL
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
 clear
-echo ""
-echo " This Feature Can Only Be Used According To VPS Data With This Autoscript"
-echo " Please Insert VPS Data Backup Link To Restore The Data"
-echo ""
+figlet "Restore" | lolcat
+echo "This Feature Can Only Be Used According To Vps Data With This Autoscript"
+echo "Please input link to your vps data backup file."
+echo "You can check it on your email if you run backup data vps before."
 read -rp "Link File: " -e url
-cd
-mkdir -p /root/backup
 wget -O backup.zip "$url"
-unzip backup.zip  &> /dev/null
+unzip backup.zip
 rm -f backup.zip
 sleep 1
 echo Start Restore
 cd /root/backup
-echo -e "[ ${green}INFO${NC} ] Start Restore . . . "
-cp -r passwd /etc/ &> /dev/null
-cp -r group /etc/ &> /dev/null
-cp -r shadow /etc/ &> /dev/null
-cp -r xray /etc/xray/config.json &> /dev/null
-cp -r ssh /etc/xray/ssh &> /dev/null
-cp -r idchat /usr/bin/idchat &> /dev/null
-cp -r token /usr/bin/token &> /dev/null
-cp -r id /etc/per/id &> /dev/null
-cp -r token2 /etc/per/token &> /dev/null
-cp -r loginid /etc/perlogin/id &> /dev/null
-cp -r logintoken /etc/perlogin/token &> /dev/null
-cp -r public_html /home/vps/ &> /dev/null
-cp -r gshadow /etc/ &> /dev/null
-cp -r sshx /etc/xray/ &> /dev/null
-cp -r vmess /etc/ &> /dev/null
-cp -r vless /etc/ &> /dev/null
-cp -r trojan /etc/ &> /dev/null
-cp -r issue /etc/issue.net &> /dev/null
-echo ""
-echo -e "[ ${green}INFO${NC} ] VPS Data Restore Complete !"
-echo ""
-echo -e "[ ${green}INFO${NC} ] Restart All Service"
-systemctl restart xray
-systemctl restart nginx
-cd
-rm -rf *
-sleep 0.5
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp chap-secrets /etc/ppp/
+cp passwd1 /etc/ipsec.d/passwd
+cp -r crot /var/lib/
+cp -r sstp /home/
+cp -r xray /etc/
+cp -r trojan-go /etc/
+cp -r shadowsocksr /usr/local/
+cp -r public_html /home/vps/
+cp crontab /etc/
+strt
+rm -rf /root/backup
+rm -f backup.zip
+echo "Restore Berhasil!!!" | lolcat
