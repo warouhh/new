3665835b1a92195907164af3a769d27bb476f21e
warouhh <136374223+warouhh@users.noreply.github.com>
2024-07-16 02:55:31 +0700

    Update backup.sh

--- menu/backup.sh
+++ menu/backup.sh
@@ -1,65 +1,81 @@
 #!/bin/bash
-# SL
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-clear
-IP=$(wget -qO- ipinfo.io/ip);
-date=$(date +"%Y-%m-%d")
-clear
-email=$(cat /home/email)
-if [[ "$email" = "" ]]; then
-echo "Masukkan Email Untuk Menerima Backup"
-read -rp "Email : " -e email
-cat <<EOF>>/home/email
-$email
-EOF
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+colornow=$(cat /etc/rmbl/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m"
+export COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+ipsaya=$(curl -s -4 icanhazip.com)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/awanklod/izin_new/main/ip"
+checking_sc() {
+useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
+if [[ $date_list < $useexp ]]; then
+echo -ne
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1│ $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  $COLOR1│"
+echo -e "$COLOR1│   ${yl}Your VPS${NC} $ipsaya \033[0;36mHas been Banned${NC}      $COLOR1│"
+echo -e "$COLOR1│     ${yl}Buy access permissions for scripts${NC}          $COLOR1│"
+echo -e "$COLOR1│             \033[0;32mContact Your Admin ${NC}                 $COLOR1│"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+exit
 fi
+}
+checking_sc
 clear
-figlet "Backup"
+IP=$(wget -qO- ipv4.icanhazip.com);
+date=$(date +"%Y-%m-%d")
 echo "Mohon Menunggu , Proses Backup sedang berlangsung !!"
 rm -rf /root/backup
 mkdir /root/backup
-cp /etc/passwd backup/
-cp /etc/group backup/
-cp /etc/shadow backup/
-cp /etc/gshadow backup/
-cp -r /etc/xray backup/xray
-cp -r /root/nsdomain backup/nsdomain
-cp -r /etc/slowdns backup/slowdns
-cp -r /home/vps/public_html backup/public_html
+cp -r /etc/passwd /root/backup/ &> /dev/null
+cp -r /etc/group /root/backup/ &> /dev/null
+cp -r /etc/shadow /root/backup/ &> /dev/null
+cp -r /etc/gshadow /root/backup/ &> /dev/null
+cp -r /usr/bin/idchat /root/backup/ &> /dev/null
+cp -r /usr/bin/token /root/backup/ &> /dev/null
+cp -r /etc/per/id /root/backup/ &> /dev/null
+cp -r /etc/per/token /root/backup/token2 &> /dev/null
+cp -r /etc/perlogin/id /root/backup/loginid &> /dev/null
+cp -r /etc/perlogin/token /root/backup/logintoken &> /dev/null
+cp -r /etc/xray/config.json /root/backup/xray &> /dev/null
+cp -r /etc/xray/ssh /root/backup/ssh &> /dev/null
+cp -r /etc/trojan-go/config.json /root/backup/trojan-go &> /dev/null
+#cp -r /etc/trojan-go/trgo /root/backup/trgo &> /dev/null
+cp -r /etc/xray/noob /root/backup/noob &> /dev/null
+cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
+cp -r /etc/xray/sshx /root/backup/sshx &> /dev/null
+cp -r /etc/vmess /root/backup/vmess &> /dev/null
+cp -r /etc/vless /root/backup/vless &> /dev/null
+cp -r /etc/trojan /root/backup/trojan &> /dev/null
+#cp -r /etc/trgo /root/backup/trgo &> /dev/null
+cp -r /etc/trojan-go /root/backup/trojan-go &> /dev/null
+cp -r /etc/noobz /root/backup/noobz &> /dev/null
+cp -r /etc/issue.net /root/backup/issue &> /dev/null
 cd /root
 zip -r $IP-$date.zip backup > /dev/null 2>&1
 rclone copy /root/$IP-$date.zip dr:backup/
 url=$(rclone link dr:backup/$IP-$date.zip)
 id=(`echo $url | grep '^https' | cut -d'=' -f2`)
 link="https://drive.google.com/u/4/uc?id=${id}&export=download"
-echo -e "
-Detail Backup 
-==================================
-IP VPS        : $IP
-Link Backup   : $link
-Tanggal       : $date
-==================================
-" | mail -s "Backup Data" $email
 rm -rf /root/backup
 rm -r /root/$IP-$date.zip
 clear
 echo -e "
-Detail Backup 
+Detail Backup
 ==================================
 IP VPS        : $IP
 Link Backup   : $link
 Tanggal       : $date
 ==================================
 "
-echo "Silahkan cek Kotak Masuk $email"
+echo "Silahkan di save link diatas"
+echo -e ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
