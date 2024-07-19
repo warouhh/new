d0f363796751dff210dd675a058ba09d1371d85f
warouhh <136374223+warouhh@users.noreply.github.com>
2024-07-20 05:14:28 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -229,12 +229,16 @@ socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
 
 [dropbear]
-accept = 777
+accept = 222
 connect = 127.0.0.1:22
 
 [dropbear]
-accept = 445
+accept = 777
 connect = 127.0.0.1:109
+
+[ws-stunnel]
+accept = 2096
+connect = 700
 
 [openvpn]
 accept = 442
