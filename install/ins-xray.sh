03c866187dca9d135d1799fbb25b53b8fad6393d
warouhh <136374223+warouhh@users.noreply.github.com>
2024-07-22 09:55:08 +0700

    Update ins-xray.sh

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -410,6 +410,8 @@ cat >/etc/nginx/conf.d/xray.conf <<EOF
     server {
              listen 80;
              listen [::]:80;
+              listen 2096;
+             listen [::]:2096;
              listen 443 ssl http2 reuseport;
              listen [::]:443 http2 reuseport;
              server_name *.$domain;
