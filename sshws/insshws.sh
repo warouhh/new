4105a9131533e906b4d5e5a2514b4681531047ca
warouhh <136374223+warouhh@users.noreply.github.com>
2024-07-25 07:10:16 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -4,7 +4,7 @@ url="https://raw.githubusercontent.com/warouhh/new/main"
 cd
 
 #Install Script Websocket-SSH Python
-#wget -O /usr/local/bin/edu-proxy ${url}/sshws/https.py
+wget -O /usr/local/bin/edu-proxy ${url}/sshws/https.py
 wget -O /usr/local/bin/ws-dropbear ${url}/sshws/dropbear-ws.py.txt
 wget -O /usr/local/bin/ws-stunnel ${url}/sshws/ws-stunnel.txt
 wget -O /usr/local/bin/edu-proxyovpn ${url}/sshws/ovpn.py
@@ -15,10 +15,10 @@ wget -O /usr/local/bin/edu-proxyovpn ${url}/sshws/ovpn.py
 chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 chmod +x /usr/local/bin/edu-proxyovpn
-chmod +x /usr/local/bin/ws-openssh
+#chmod +x /usr/local/bin/ws-openssh
 
 #System Direcly dropbear Websocket-SSH Python
-#wget -O /etc/systemd/system/edu-proxy.service ${url}/sshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
+wget -O /etc/systemd/system/edu-proxy.service ${url}/sshws/http.service && chmod +x /etc/systemd/system/edu-proxy.service
 
 #System Dropbear Websocket-SSH Python
 wget -O /etc/systemd/system/ws-dropbear.service ${url}/sshws/service-wsdropbear.txt && chmod +x /etc/systemd/system/ws-dropbear.service
@@ -38,9 +38,9 @@ systemctl daemon-reload
 
 #Enable & Start & Restart directly dropbear
 #systemctl daemon-reload
-#systemctl enable edu-proxy.service
-#systemctl start edu-proxy.service
-#systemctl restart edu-proxy.service
+systemctl enable edu-proxy.service
+systemctl start edu-proxy.service
+systemctl restart edu-proxy.service
 
 #Enable & Start & Restart ws-dropbear service
 systemctl enable ws-dropbear.service
